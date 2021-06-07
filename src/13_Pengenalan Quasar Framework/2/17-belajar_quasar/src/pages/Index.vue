eb5c1d9736c8e4ad7c938115ec17adade265924e
kinanpermata <kinanti.permat@gmail.com>
2021-06-07 15:07:20 +0700

    13

---
+++ src/13_Pengenalan Quasar Framework/2/17-belajar_quasar/src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <h3>{{ hello }}</h3>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      hello: "Halo, Nama Saya Kinan dengan NIM 1841720022"
+    }
+  }
+}
+</script>
